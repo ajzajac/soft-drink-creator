f60bf9b9c7610e8f22315d352de1634170ef697a
ajzajac <ajzajac@icloud.com>
2020-08-03 21:18:05 -0400

    beverage controller CRU

--- app/controllers/beverages_controller.rb
+++ app/controllers/beverages_controller.rb
@@ -1,2 +1,29 @@
 class BeveragesController < ApplicationController
+
+    def index
+        @beverages = Beverage.all
+        render json: @beverages
+    end
+
+    def show
+        @beverage = Beverage.find(params[:id])
+        render json: @beverage, status :ok
+    end
+
+    def create
+        @beverage = Beverage.create(beverageParams)
+    end
+
+    def update
+        @beverage = Beverage.find(params[:id])
+        @beverage.update(beverageParams)
+        render json: {beverage: @beverage}
+    end
+
+    private
+
+    def beverageParams
+        params.require(:beverage).permit(:id, :name, :water, :size)
+    end
+
 end
