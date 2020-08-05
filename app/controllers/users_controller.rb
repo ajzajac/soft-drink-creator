5dd10a16991ea25fa96a92b9161e5cf882422c53
ajzajac <ajzajac@icloud.com>
2020-08-05 17:34:57 -0400

    added user has_many, user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,23 @@
 class UsersController < ApplicationController
 
-    def create
-        user = User.new(
-          username: params[:username],
-          password: params[:password],
-          email: params[:email],
-        )
-    
-        if user.save
-          token = encode_token(user.id)
-          render json: {user: user, token: token}
-        else
-          render json: {errors: user.errors.full_messages}
-        end
+  def index
+    @users = User.all
+    render json: {users: @users}
+  end
+
+  def create
+      user = User.new(
+        username: params[:username],
+        password: params[:password],
+        email: params[:email],
+      )
+  
+      if user.save
+        token = encode_token(user.id)
+        render json: {user: user, token: token}
+      else
+        render json: {errors: user.errors.full_messages}
       end
+    end
 
 end
