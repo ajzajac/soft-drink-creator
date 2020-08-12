a0fb861735e7d940b189ab596c9755af2b95a66d
ajzajac <ajzajac@icloud.com>
2020-08-12 12:01:14 -0400

    validated beverage params

--- app/models/beverage.rb
+++ app/models/beverage.rb
@@ -1,4 +1,9 @@
 class Beverage < ApplicationRecord
     has_many :ingredients
     belongs_to :user
+
+    validates :name, presence: true
+    validates :base_flavor, presence: true
+    validates :secondary_flavor, presence: true
+    validates :water_type, presence: true
 end
